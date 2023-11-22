08ec29b8508032d96b27c8d0a907a8eb65c02afd
Ujjwal Sharma <ujjwal.sharma@dhiway.com>
2023-11-22 14:49:30 +0530

    added more lazygit functions

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -10,8 +10,14 @@ M.general = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<C-d>"] = { "Find Under", "description for Find Under" },
     ["<leader>fp"] = { "<cmd> ProjectMgr<CR>", "Open Projects"},
-    ["<leader>gg"] = { "<cmd> :LazyGit<CR>", "open lazygit" },
+    ["gg"] = { "<cmd> :LazyGit<CR>", "open lazygit" },
+    ["<leader>gf"] = { "<cmd> :LazyGitFilter<CR>", "lazygit commits" },
+    ["gG"] = { "<cmd> :LazyGitCurrentFile<CR>", "open lazygit for current" },
+    ["<leader>gF"] = { "<cmd> :LazyGitFilter<CR>", "lazygit commits for current" },
     ["<leader>db"] = { "<cmd> DapToggleBreakpoint <CR>", "debugger toggle breakpoints" },
+    ["<leader>gt"] = {function ()
+       require("telescope").extensions.lazygit.lazygit()
+    end, "open lazygit telescope"},
     ["<leader>dus"] = {
       function ()
         local widgets = require('dap.ui.widgets');
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -119,7 +119,11 @@ local plugins = {
         -- optional for floating window border decoration
         dependencies = {
             "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
         },
+         config = function()
+            require("telescope").load_extension("lazygit")
+        end,
     },
   -- rust plugins
   {
