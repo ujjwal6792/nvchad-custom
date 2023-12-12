173fc6bf809570011fc2a1f1b5edcda085fa9e6f
Ujjwal Sharma <ujjwal.sharma@dhiway.com>
2023-12-12 18:31:16 +0530

    [feat] added package-info plugin

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -230,6 +230,17 @@ local plugins = {
       require("neoscroll").setup {}
     end,
   },
+  {
+    "vuki656/package-info.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    ft = { "json" },
+    config = function()
+      require("package-info").setup {
+        hide_unstable_versions = true,
+        hide_up_to_date = true,
+      }
+    end,
+  },
 }
 
 return plugins
